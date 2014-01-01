ca26aec67e452cd5f93aafaacf950abbb813103b
Joshua Lowe <joshua.lowe.dev@gmail.com>
2013-12-31 18:29:20 -0600

    New files

---
+++ lib/WebService/MusicBrainz2/Response/Attribute.pm
@@ -0,0 +1,51 @@
+package WebService::MusicBrainz2::Response::Attribute;
+
+use strict;
+use base 'Class::Accessor';
+
+our $VERSION = '0.23';
+
+=head1 NAME
+
+WebService::MusicBrainz2::Response::Attribute
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+All the methods listed below are accessor methods.  They can take a scalar argument to set the state of the object or without and argument, they will return that state if it is available.
+
+=head2 count
+
+=head2 text
+
+=cut
+
+__PACKAGE__->mk_accessors(qw/text count/);
+
+=head1 AUTHOR
+
+=over 4
+
+=item Joshua Lowe <joshua.lowe.dev@gmail.com>
+=item Bob Faist <bob.faist@gmail.com>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2013 by Joshua Lowe
+Copyright 2006-2009 by Bob Faist
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+http://wiki.musicbrainz.org/XMLWebService
+
+=cut
+
+1;
---
+++ lib/WebService/MusicBrainz2/Response/CoverArtArchive.pm
@@ -0,0 +1,53 @@
+package WebService::MusicBrainz2::Response::CoverArtArchive;
+
+use strict;
+use base 'Class::Accessor';
+
+our $VERSION = '0.23';
+
+=head1 NAME
+
+WebService::MusicBrainz2::Response::CoverArtArchive
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+All the methods listed below are accessor methods.  They can take a scalar argument to set the state of the object or without and argument, they will return that state if it is available.
+
+=head2 artwork
+
+=head2 count
+
+=head2 front
+
+=head2 back
+
+=cut
+
+__PACKAGE__->mk_accessors(qw/artwork count front back/);
+
+=head1 AUTHOR
+
+=over 4
+
+=item Bob Faist <bob.faist@gmail.com>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2006-2009 by Bob Faist
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+http://wiki.musicbrainz.org/XMLWebService
+
+=cut
+
+1;
